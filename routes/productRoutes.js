a8063c3b9c1ee849f19055358e88ff1890044afd
SHivam-star33 <pixalunknown2003@gmail.com>
2023-05-25 14:31:12 +0530

    Updates

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.route('/products').get(getAllProducts)
 router.route('/admin/products/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProducts)
 router.route('/admin/products/:id').put(isAuthenticatedUser,authorizeRoles("admin"), updateProducts).delete(isAuthenticatedUser,authorizeRoles("admin"), deleteProducts)
-router.route('/products/:id').get(getProductDetails)
+router.route('/product/:id').get(getProductDetails)
 router.route('/review').put(isAuthenticatedUser, createProductReview)
 router.route('/reviews').get(getAllreviewsOfSingleProduct).delete(isAuthenticatedUser, deleteReviewer)
 module.exports = router
