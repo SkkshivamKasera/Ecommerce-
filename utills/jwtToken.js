79c0424448b79ba19115afca1232085a0055e896
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2023-08-05 23:19:02 +0530

    update jwtToken.js

--- utills/jwtToken.js
+++ utills/jwtToken.js
@@ -2,15 +2,13 @@ const ErrorHandler = require('./errorHandler')
 const sendToken = (user, req, res, next) => {
     try{const token = user.getJwtToken()
     console.log( process.env.COOKIE_EXPIRE )
-    const options = {
+    res.cookie("token", token, {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure: false
-    }
-
-    res.cookie("token", token, options).json({
+        secure: true
+    }).json({
         success: true,
         message: "🎉🎉🎉Successfully🎉🎉🎉",
         token,
