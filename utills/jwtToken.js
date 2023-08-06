2d4a0ac52482789610deca83cfed5b869eb3fe4d
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2023-08-06 14:50:04 +0530

    UPDATE

--- utills/jwtToken.js
+++ utills/jwtToken.js
@@ -9,7 +9,8 @@ const sendToken = (user, req, res, next) => {
         res.cookie("token", token, {
             expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            secure: secureFlag // Set the secure flag based on the environment
+            secure: true, // Set the secure flag based on the environment
+            domain: "https://ecommerce-hc5g.onrender.com"
         }).json({
             success: true,
             message: "🎉🎉🎉Successfully🎉🎉🎉",
