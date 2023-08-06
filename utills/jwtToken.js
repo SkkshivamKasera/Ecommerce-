6acf2210938644589d8927f636c48617d275157a
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2023-08-06 12:17:25 +0530

    Update

--- utills/jwtToken.js
+++ utills/jwtToken.js
@@ -1,21 +1,24 @@
-const ErrorHandler = require('./errorHandler')
 const sendToken = (user, req, res, next) => {
-    try{const token = user.getJwtToken()
-    console.log( process.env.COOKIE_EXPIRE )
-    res.cookie("token", token, {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-        secure: false
-    }).json({
-        success: true,
-        message: "🎉🎉🎉Successfully🎉🎉🎉",
-        token,
-        user
-    })}catch(error){
-        return next(new ErrorHandler(error.message))
+    try {
+        const token = user.getJwtToken();
+        console.log(process.env.COOKIE_EXPIRE);
+        
+        // Determine the secure value based on the environment
+        const secureFlag = req.secure || req.headers['x-forwarded-proto'] === 'https';
+        
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: secureFlag // Set the secure flag based on the environment
+        }).json({
+            success: true,
+            message: "🎉🎉🎉Successfully🎉🎉🎉",
+            token,
+            user
+        });
+    } catch (error) {
+        return next(new ErrorHandler(error.message));
     }
-}
+};
 
-module.exports = sendToken
+module.exports = sendToken;
